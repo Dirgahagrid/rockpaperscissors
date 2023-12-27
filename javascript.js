e7043635d7ba6df6b404794c692a68f89f9850cb
Dirgahagrid <139720494+Dirgahagrid@users.noreply.github.com>
2023-12-26 20:45:00 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,26 +1,84 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = '';
+let message = '';
+
+function player(elem) {
+    playerSelection = elem.id;
+    console.log(elem.id);
+    if(playerScore >= 5 || computerScore >= 5) {
+        alert("Game over! Play again");
+        computerScore = 0;
+        playerScore = 0;
+        document.getElementById("computer").innerHTML = computerScore;
+        document.getElementById("player").innerHTML = playerScore;
+        document.getElementById("message").innerHTML = '';
+    }
+    else {
+        game(playerSelection);
+    }
+}
+
+
 function getComputerChoice(randomInt) {
-    let computerHand;
     switch(randomInt) {
         case 1:
-            computerHand = "rock";
+            computerSelection = "rock";
             break;
         case 2:
-            computerHand = "paper";
+            computerSelection = "paper";
             break;
         case 3:
-            computerHand = "scissors";
+            computerSelection = "scissors";
             break;
     }
-console.log(computerHand);
 }
-let computerSelection = getComputerChoice(Math.floor(Math.random() *3) + 1);
 
+function game(playerSelection) {
+        let computerSelection = getComputerChoice(Math.floor(Math.random() *3) + 1);
+        let result = playRound(playerSelection, computerSelection);
 
-let playerPrompt = window.prompt("Please type in Rock, Paper, or Scissors");
-let playerSelection = playerPrompt.toLowerCase();
-console.log(playerSelection);
-playRound(playerSelection, computerSelection);
+    }
 
 function playRound(player, computer) {
 
+    if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+      ) {
+            playerScore++;
+            result = 'player';
+            document.getElementById("computer").innerHTML = computerScore;
+            document.getElementById("player").innerHTML = playerScore;
+            document.getElementById("player-Selection").innerHTML = playerSelection;
+            document.getElementById("computer-Selection").innerHTML = computerSelection;
+            document.getElementById("message").innerHTML = `You selected ` + playerSelection + ` and that beats ` + computerSelection + ` you win!`;
+      }
+
+    else if (
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')
+      ) {
+        computerScore++;
+        result = 'computer';
+        document.getElementById("computer").innerHTML = computerScore;
+        document.getElementById("player").innerHTML = playerScore;
+        document.getElementById("player-Selection").innerHTML = playerSelection;
+        document.getElementById("computer-Selection").innerHTML = computerSelection;
+        document.getElementById("message").innerHTML = `You selected ` + playerSelection + ` and the computer selected ` + computerSelection + ` you lose!` ;
+    }
+    else {
+        result =  "tie";
+        document.getElementById("computer").innerHTML = computerScore;
+        document.getElementById("player").innerHTML = playerScore;
+        document.getElementById("player-Selection").innerHTML = playerSelection;
+        document.getElementById("computer-Selection").innerHTML = computerSelection;
+        document.getElementById("message").innerHTML = `tie`;
+    }
 }
+
+
+
+
